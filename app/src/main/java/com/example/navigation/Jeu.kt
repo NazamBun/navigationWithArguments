891ef437a76cb9f461350810969407dfa91e0533
Nazam <bun.nazam@gmail.com>
2023-12-04 20:27:12 +0100

    separate line

--- app/src/main/java/com/example/navigation/Jeu.kt
+++ app/src/main/java/com/example/navigation/Jeu.kt
@@ -1,1 +1,12 @@
-package com.example.navigationdata class Jeu(    val id: Int,    val nom: String)val listJeux =  listOf(    Jeu(1, "SuperAventure"),    Jeu(2, "CourseEclair"),    Jeu(3, "PuzzleMagique"))
+package com.example.navigation
+
+data class Jeu(
+    val id: Int,
+    val nom: String
+)
+
+val listJeux =  listOf(
+    Jeu(1, "SuperAventure"),
+    Jeu(2, "CourseEclair"),
+    Jeu(3, "PuzzleMagique")
+)
